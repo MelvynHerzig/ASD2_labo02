13d89cafcd60d7e186ac9406b8ef8fe576509a57
MelvynHerzig <34660483+MelvynHerzig@users.noreply.github.com>
2020-10-07 15:12:19 +0200

    Exo2 fin deuxième séance

--- L2b/CFC.h
+++ L2b/CFC.h
@@ -1,6 +1,7 @@
 /*
  * Author: Antoine Rochat
  * Created on 09 sep 2020
+ * Modified on 07. aug 2020 by Berney Alec, Forestier Quentin, Herzig Melvyn
  */
 
 #ifndef CFC_H
@@ -12,45 +13,83 @@
 
 // Classe mettant en oeuvre les composantes fortement connexes
 template<typename GraphType>
-class CFC {
+class CFC
+{
 protected:
-    // numéro de la CFC à laquelle appartient chaque sommet (-1 si pas encore assigné)
-    std::vector<int> id;
+   // numéro de la CFC à laquelle appartient chaque sommet (-1 si pas encore assigné)
+   std::vector<int> id;
 
-    // nombre de composantes connexes
-    int count;
+   // nombre de composantes connexes
+   int count;
 
 public:
-    // v et w sont-ils connectés ?
-    bool Connected(int v, int w) {
-        return Id(v) == Id(w);
-    }
 
-    // à quelle CFC appartient v ?
-    int Id(int v) {
-        return id.at(v);
-    }
+   /**
+    * @brief Définit si deux sommets sont connectés.
+    * @param v Premier sommet.
+    * @param w Second sommet.
+    * @return Vrai si les deux sommets appartienent à la même compsante fortement connexe.
+    */
+   bool Connected (int v, int w)
+   {
+      return Id(v) == Id(w);
+   }
 
-    // combien y a-t-il de CC ?
-    int Count() {
-        return count;
-    }
+
+   /**
+    * @brief Cherche le numéro de composante fortement connexe du sommet.
+    * @param v Sommet.
+    * @return Retourne le numéro de la composante fortement connexe associé à v.
+    */
+   int Id (int v)
+   {
+      return id.at(v);
+   }
+
+   /**
+    * @brief Retourne le nombre de composantes fortement connexes du graphe.
+    * @return Le nombre de composantes fortement connexes.
+    */
+   int Count ()
+   {
+      return count;
+   }
 };
 
 template<typename GraphType>
-class KosarajuSharirCFC : public CFC<GraphType> {
+class KosarajuSharirCFC : public CFC<GraphType>
+{
 
 public:
 
-    // Constructeur a partir du graphe g et du graphe inverse de g
-    KosarajuSharirCFC(const GraphType& g, const GraphType& gReverse) {
-/****
-*
-*  A IMPLEMENTER
-*  Implémenter l'algorithme de KosarajuSharir en utilisant le parcours en profondeur fourni
-*
-****/
-    }
+   /**
+    * @brief Définit les conposantes fortement connexes pour le graphe g et son transposé.
+    * @param g Graphe.
+    * @param gReverse Graphe transposé de g.
+    */
+   KosarajuSharirCFC (const GraphType &g, const GraphType &gReverse)
+   {
+      std::vector<int> postOrdre (g.V());
+      std::fill(this->id.begin(), this->id.end(), -1);
+      unsigned noCFC = 0;
+
+      DFSIter<GraphType> DFS  {g};
+      DFSIter<GraphType> DFST {gReverse};
+
+      DFST.visitGraph([](int v) {}, [&postOrdre](int v){postOrdre.push_back(v);});
+      std::reverse(postOrdre.begin(), postOrdre.end());
+
+      for(int i = 0; i < postOrdre.size(); ++i)
+      {
+         if(this->id[postOrdre[i]] == -1)
+         {
+            DFS.visit(postOrdre[i],[this, &noCFC](int v){this->id[v] = noCFC;}, [](int v) {});
+            ++noCFC;
+         }
+      }
+
+      this->count = noCFC;
+   }
 };
 
 #endif
--- L2b/SocialNetwork.h
+++ L2b/SocialNetwork.h
@@ -3,6 +3,7 @@
  * Author: Antoine Rochat
  *
  * Created on 08. sept 2020
+ * Modified on 07. aug 2020 by Berney Alec, Forestier Quentin, Herzig Melvyn
  */
 
 #ifndef ASD2_SocialNetwork_h
@@ -16,73 +17,62 @@
 #include "DiGraph.h"
 
 template<typename GraphType>
-class SocialNetwork : public SymbolGraph<GraphType> {
+class SocialNetwork : public SymbolGraph<GraphType>
+{
 
-    typedef SymbolGraph<GraphType> BASE;
-
-/****
-*
-*  AJOUTEZ ICI VOS EVENTUELLES STRUCTURES PRIVEES
-*
-****/
+   typedef SymbolGraph<GraphType> BASE;
 
 public:
-    SocialNetwork(const std::string& filename, char delim = '/')
-/****
-*
-*  A IMPLEMENTER
-*
-****/
-    {}
+   SocialNetwork (const std::string &filename, char delim = '/') : BASE(filename, delim)
+   {}
 
-    // retourne les personnes auxquelles la personne "name" est abonnée
-    std::vector<std::string> personsSubscribedBy(const std::string& name) const {
-/****
-*
-*  A IMPLEMENTER
-*
-****/
-    }
+   // retourne les personnes auxquelles la personne "name" est abonnée
+   std::vector<std::string> personsSubscribedBy (const std::string &name) const
+   {
+      return BASE::adjacent(name);
+   }
 
-    // retourne les personnes qui sont abonnées à la personne "name"
-    std::vector<std::string> personsWhoFollows(const std::string& name) const {
-/****
-*
-*  A IMPLEMENTER
-*  Cette méthode doit avoir une compléxité en O(S), où S est le nombre de personnes qui sont abonnées à la personne "name"
-*
-****/
-    }
+   // retourne les personnes qui sont abonnées à la personne "name"
+   std::vector<std::string> personsWhoFollows (const std::string &name) const
+   {
+      std::vector<std::string> v;
+      return v;
+      //return grapheTranspose.adjacent(name);
+   }
 
-    // determine si la personne subscriber suit la personne personFollowed
-    bool isFollowing(const std::string& subscriber, const std::string& personFollowed) const {
-/****
-*
-*  A IMPLEMENTER
-*
-****/
-    }
+   // determine si la personne subscriber suit la personne personFollowed
+   bool isFollowing (const std::string &subscriber, const std::string &personFollowed) const
+   {
+      std::vector<std::string> adjacent = BASE::adjacent(subscriber);
+      return std::find(adjacent.begin(), adjacent.end(), personFollowed) != adjacent.end();
+   }
 
-    // ajoute un nouvel abonnement au réseau social, subscriber suit personToFollow
-    void addSubscription(const std::string& subscriber, const std::string& personToFollow) {
-/****
-*
-*  A IMPLEMENTER
-*  Ne modifie pas notre reseau social si subscriber suit deja personToFollow
-*
-****/
-    }
+   // ajoute un nouvel abonnement au réseau social, subscriber suit personToFollow
+   void addSubscription (const std::string &subscriber, const std::string &personToFollow)
+   {
+      if(isFollowing(subscriber, personToFollow)) return;
 
-    // retourne les personnes faisant partie du cercle de relations de la personne "name"
-    std::vector<std::string> relationCircle(const std::string& name) const {
-/****
-*
-*  A IMPLEMENTER
-*  Utiliser la classe KosarajuSharirCFC afin de trouver le cercle de relations d'une personne
-*  (correspond aux personnes se trouvant dans la même composante fortement connexe)
-*
-****/
-    }
+      BASE::g->addEdge(BASE::index(subscriber), BASE::index(personToFollow));
+      //grapheTranspose.addEgde(graphe.index(personToFollow), graphe.index(subscriber));
+   }
+
+   // retourne les personnes faisant partie du cercle de relations de la personne "name"
+   std::vector<std::string> relationCircle (const std::string &name) const
+   {
+      KosarajuSharirCFC<GraphType> cfc = {*BASE::g, BASE::g->reverse()};
+      int idBase = BASE::index(name);
+      std::vector<std::string> amitie;
+
+      for(size_t i = 0; i < BASE::g->V(); ++i)
+      {
+         if(i != idBase && cfc.Connected(i, idBase))
+         {
+            amitie.push_back(BASE::symbol(i));
+         }
+      }
+
+      return amitie;
+   }
 };
 
 #endif
